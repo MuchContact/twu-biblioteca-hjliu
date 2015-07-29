f5655fd6fda4f2e3acaf0d283392f5a50ef966b0
MuchContact <422549477@qq.com>
2015-07-29 16:56:26 +0800

    validate that each book in Biblioteca has detail information such as author and publish year.

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 import static com.twu.biblioteca.Biblioteca.getAllBooks;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class BibliotecaTest {
+
+    private final String bookTitle = "Agile Game Development with Scrum";
+
     @BeforeClass
     public static void setUp() throws Exception {
         Biblioteca.fillBibliotecaWithBooks();
@@ -25,16 +29,24 @@ public class BibliotecaTest {
     @Test
     public void should_list_all_available_books() throws Exception {
         Customer guest = new Customer("guest");
-        String title = "Agile Game Development with Scrum";
-        assertTrue(guest.checkout(title));
+        assertTrue(guest.checkout(bookTitle));
         assertEquals(5, Biblioteca.getAllAvailableBooks().size());
-        assertTrue(guest.returnBook(title));
+        assertTrue(guest.returnBook(bookTitle));
     }
 
     @Test
     public void should_contain_detail_info() throws Exception {
-        Book book = Biblioteca.getBook("Agile Game Development with Scrum");
+        Book book = Biblioteca.getBook(bookTitle);
         assertEquals("Clinton Keith", book.getAuthor());
         assertEquals(2010, book.getPublishedYear());
+    }
+
+    @Test
+    public void should_each_book_contain_detail_info() throws Exception {
+        List<Book> allAvailableBooks = Biblioteca.getAllAvailableBooks();
+        allAvailableBooks.stream().forEach((book)
+                ->{ assertNotNull(book.getAuthor());
+                    assertNotNull(book.getPublishedYear());
+                });
     }
 }
