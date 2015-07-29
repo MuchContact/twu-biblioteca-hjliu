c4cb5fdb6ddb4dac17a0de672bbc0c191209d39a
MuchContact <422549477@qq.com>
2015-07-29 18:32:01 +0800

    add support for "List Books" and "Quit" operations

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,39 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca!");
+        System.out.println("");
+        System.out.println("All books are as follows:");
+        List<Book> bookList = Biblioteca.getAllBooks();
+        final int[] index = {1};
+        bookList.stream().forEach((book) -> {
+            System.out.println(index[0] +". "+book.getTitle());
+            index[0]++;});
+        Scanner in = new Scanner(System.in);
+        System.out.println("Operation Options: ");
+        System.out.println("    1: List Books");
+        System.out.println("(Notes: )");
+        while(true){
+            String next = in.nextLine();
+            if(next.trim().equalsIgnoreCase("List Books")){
+                System.out.println("All books are as follows:");
+                List<Book> tmp = Biblioteca.getAllBooks();
+                tmp.stream().forEach((book) -> {
+                    System.out.println(book.getTitle());});
+                continue;
+            }
+            if(next.trim().equalsIgnoreCase("Quit")){
+                break;
+            }
+
+            System.out.println("Select a valid option!");
+        }
+
     }
 
-    public String startApp() {
-        return "Welcome to Biblioteca!";
-    }
 }
